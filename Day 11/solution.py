3880ccbf83ae32784534c84734d6a84d540306fd
Shan-Wei Mensing <SFLMensing@hotmail.com>
2024-06-14 01:23:17 +0200

    Day 11 completed (this code is garbage)!

--- Day 11/solution.py
+++ Day 11/solution.py
@@ -1,26 +1,20 @@
 def main():
-    with open("example.txt") as f:
+    with open("input.txt") as f:
         data_image = f.read().splitlines()
     
     expanded_universe = expand_universe(data_image)
     galaxies_dict = get_galaxies(expanded_universe)
     
     solution1(expanded_universe, galaxies_dict)
-    # solution2(data_image)
+    solution2(expanded_universe, galaxies_dict)
 
 
 def solution1(data, galaxies):
-
     print(f"Solution 1: {get_total_distance(data, galaxies)}")
 
 
-def solution2(data):
-    expanded_universe = expand_universe(data)
-    galaxies_dict = get_galaxies(expanded_universe)
-
-    print(f"!!! Solution 2 galaxies: {galaxies_dict}")
-
-    print(f"Solution 2: {get_total_distance(expanded_universe, galaxies_dict)}")
+def solution2(data, galaxies):
+    print(f"Solution 2: {get_total_distance(data, galaxies, old_galaxies=True)}")
 
 
 def expand_universe(data_image):
@@ -62,17 +56,36 @@ def get_galaxies(data_image):
 
 
 def get_total_distance(data_image, galaxies_dict, old_galaxies=False):
+    OLD_GALAXY_DISTANCE = 999999
     unprocessed_galaxies = [x for x in range(len(galaxies_dict))]
     total_distance = 0
 
-    if old_galaxies:
-        pass
-    else:
-        while unprocessed_galaxies:
-            current_galaxy = unprocessed_galaxies.pop()
-            for other_galaxy in unprocessed_galaxies:
-                total_distance += abs(galaxies_dict[current_galaxy][0]-galaxies_dict[other_galaxy][0])
-                total_distance += abs(galaxies_dict[current_galaxy][1]-galaxies_dict[other_galaxy][1])
+    while unprocessed_galaxies:
+        current_galaxy = unprocessed_galaxies.pop()
+        for other_galaxy in unprocessed_galaxies:
+            vertical_matrix_delta = abs(galaxies_dict[current_galaxy][0]-galaxies_dict[other_galaxy][0])
+            horizontal_matrix_delta = abs(galaxies_dict[current_galaxy][1]-galaxies_dict[other_galaxy][1])
+            if old_galaxies:
+                for row_iteration in range(vertical_matrix_delta):
+                    if galaxies_dict[current_galaxy][0] == galaxies_dict[other_galaxy][0]:
+                        continue
+                    elif galaxies_dict[current_galaxy][0] > galaxies_dict[other_galaxy][0]:
+                        row_iteration = -row_iteration
+                    if all(x=='M' for x in data_image[galaxies_dict[current_galaxy][0] + row_iteration]):
+                        total_distance += OLD_GALAXY_DISTANCE
+                    else:
+                        total_distance += 1
+                for column_iteration in range(horizontal_matrix_delta):
+                    if galaxies_dict[current_galaxy][1] == galaxies_dict[other_galaxy][1]:
+                        continue
+                    elif galaxies_dict[current_galaxy][1] > galaxies_dict[other_galaxy][1]:
+                        column_iteration = -column_iteration
+                    if data_image[galaxies_dict[current_galaxy][0]][galaxies_dict[current_galaxy][1] + column_iteration] == 'M':
+                        total_distance += OLD_GALAXY_DISTANCE
+                    else:
+                        total_distance += 1
+            else:
+                total_distance += vertical_matrix_delta + horizontal_matrix_delta
     
     return total_distance
 

2732e6d765737888c79447052f9d9a67b5c4be48
Shan-Wei Mensing <SFLMensing@hotmail.com>
2024-09-12 15:57:47 +0200

    Day 13 preperation

---
+++ Day 13/solution.py
@@ -0,0 +1,13 @@
+def main():
+    with open("input.txt") as f:
+        data_image = f.read().splitlines()
+    
+    solution1(data_image)
+
+
+def solution1(data_image):
+    pass
+
+
+if __name__ == "__main__":
+    main()

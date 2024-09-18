00b8543896940639e43261622b2b14a826fa6109
Shan-Wei Mensing <SFLMensing@hotmail.com>
2024-09-18 10:06:49 +0200

    How did I create such a good solution? Through stealing

--- Day 13/solution.py
+++ Day 13/solution.py
@@ -1,22 +1,14 @@
-from functools import cache
-from collections import deque
-from copy import deepcopy
-
-def main():
-    with open("example.txt") as f:
-        data_image = f.read()
-    
-    print(f"Solution 1: {solution1(data_image)}")
-    print(f"Solution 2: {solution2(data_image)}")
+with open("input.txt") as f:
+    data_image = list(map(str.split, f.read().split('\n\n')))
 
 
-def solution1():
-    pass
+def solver(data_map):
+    for index in range(1, len(data_map)):
+        # print(sum(top_symbol != bottom_symbol for top_row, bottom_row in zip(data_map[index:], data_map[index-1::-1]) for top_symbol,bottom_symbol in zip(top_row, bottom_row)))
+        if sum(top_symbol != bottom_symbol for top_row, bottom_row in zip(data_map[index:], data_map[index-1::-1]) for top_symbol,bottom_symbol in zip(top_row, bottom_row)) == global_smudge_value:
+            return index
+    return 0
 
 
-def solution2():
-    pass
-
-
-if __name__ == "__main__":
-    main()
+for global_smudge_value in 0,1:
+    print(sum(100*solver(data_map) + solver([*zip(*data_map)]) for data_map in data_image))

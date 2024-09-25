5f2c981fbde19040369897ac891e4bc05e4e5202
Shan-Wei Mensing <SFLMensing@hotmail.com>
2024-09-25 15:01:30 +0200

    So basically I have no idea what I'm doing

--- Day 14/solution.py
+++ Day 14/solution.py
@@ -1,42 +1,60 @@
-from collections import deque
 from functools import cache
 
 def main():
-    with open("input.txt") as f:
-        data = f.read().splitlines()
+
+    with open("example.txt") as f:
+        data = tuple(f.read().splitlines())
     
     solution1(data)
-    # solution2(data)
+    solution2(data)
 
 
-def solution1(data) -> None:
-    total_weight = 0
-    row_amt, col_amt = (len(data), len(data[0]))
+def solution1(data: tuple) -> None:
+    tilted_data = tilt(data)
+    print(f"Solution 1: {get_total_load(tilted_data)}")
 
-    column_heights_dict = {pos: 0 for pos in range(col_amt)}
-    new_data = [['.' for _ in range(col_amt)] for _ in range(row_amt)]
+
+def solution2(data) -> None:
+    tilted_data = tilt(data)
+    rotations = 3
+    while rotations - .25 > 0:
+        tilted_data = tilt(tuple([*zip(*reversed(tilted_data))]))
+        rotations -= .25
+
+    tilted_data = tuple([*zip(*reversed(tilted_data))])
+
+    for r in tilted_data:
+        print(''.join(r))
+
+    print(f"Solution 2: {get_total_load(tilted_data)}")
+
+
+@cache
+def tilt(data: tuple) -> tuple:
+    row_amount, col_amount = (len(data), len(data[0]))
+    column_heights_dict = {pos: 0 for pos in range(col_amount)}
+    new_data = [['.' for _ in range(col_amount)] for _ in range(row_amount)]
 
     for vertical_position, row in enumerate(data):
-        # print(row)
         for horizontal_position, symbol in enumerate(row):
             if symbol == 'O':
-                # print(f"'O' found on row {vertical_position}, col {horizontal_position};;; changing row {vertical_position}, col {column_heights_dict[horizontal_position]}")
                 new_data[column_heights_dict[horizontal_position]][horizontal_position] = 'O'
                 column_heights_dict[horizontal_position] += 1
             elif symbol == '#':
                 new_data[vertical_position][horizontal_position] = '#'
                 column_heights_dict[horizontal_position] = vertical_position + 1
-    
-    load_value = row_amt
-    for row in new_data:
+                
+    return new_data
+
+
+def get_total_load(data: tuple) -> int:
+    total_weight = 0
+
+    load_value = len(data)
+    for row in data:
         total_weight += row.count('O') * load_value
         load_value -= 1
-    
-    print(f"Solution 1: {total_weight}")
-
-
-def solution2(data) -> None:
-    pass
+    return total_weight
 
 
 if __name__ == "__main__":

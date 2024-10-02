523a6b3a88ed34091d82273bdbf6e929db90f6ae
Shan-Wei Mensing <SFLMensing@hotmail.com>
2024-10-02 16:33:02 +0200

    Part 1 was free lol

--- Day 15/solution.py
+++ Day 15/solution.py
@@ -1,25 +1,32 @@
-from functools import cache
-from typing import Union
-import time
-
-CUBE_LOCATIONS = dict()
-PATTERNS = dict()
-
 def main():
 
     with open("input.txt") as f:
-        data = tuple(f.read())
+        data = f.read().split(',')
     
     solution1(data)
     # solution2(data)
 
 
-def solution1(data: tuple) -> None:
-    print(data)
+def solution1(sequence: list) -> None:
+    total = 0
+    for step in sequence:
+        total += process_step(step)
+    
+    print(f"Solution 1: {total}")
 
 
 def solution2(data: tuple) -> None:
     pass
+
+
+def process_step(step: str) -> int:
+    current_value = 0
+    for character in step:
+        current_value += ord(character)
+        current_value *= 17
+        current_value %= 256
+
+    return current_value
 
 
 if __name__ == "__main__":
